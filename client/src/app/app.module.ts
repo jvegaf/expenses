1b6b714364c246e3157c8c7d149cabc92cf2b134
Jose Vega <jvega@tulotero.com>
2024-03-04 23:44:57 +0100

    users service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule,
+    GroupsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
